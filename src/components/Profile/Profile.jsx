21b75aba3ba051c9c8d728816d07ce6998601673
zargrig <zaraadevelop@gmail.com>
2020-10-17 17:58:00 +0400

    Dialogs component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div>
             <div>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
