d85a9cc8533bf39a2a3b3b1949dfa6d261c297ed
zargrig <zaraadevelop@gmail.com>
2020-10-17 15:58:59 +0400

    Init

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>new posts </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                    <div>post3</div>
+                </div>
+            </div>
+        </div>)
+};
+
+export default Profile;
